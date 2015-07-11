41af924b2cc562c2e99560992e011fe9474db7f5
Tim Hollingshead <thollingshead@esri.com>
2015-07-11 09:42:16 -0500

    Add jscs reporter

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 var notify = require('gulp-notify');
-
-module.exports = function(options) {
-	return notify(options);
+var path = require('path');
+var extend = require('util')._extend;
+var clone = function(obj) {
+	return extend({}, obj);
 };
+
+var jscs = function(options) {
+	options = clone(options);
+	options.icon = path.join(__dirname, 'assets', 'jscs.png');
+
+	return notify(function(file) {
+		if (!file.jscs || file.jscs.success) {
+			return;
+		}
+
+		var errors = file.jscs.errors.map(function(error) {
+			return error.line + ':' + error.column + '  ' + error.message;
+		});
+		options.message = errors.join('\n');
+
+		options.title = file.relative;
+		if (errors.length > 1) {
+			options.title += ' (' + errors.length + ' errors)';
+		}
+
+		return options;
+	});
+};
+
+var all = function(options) {
+	return jscs(options);
+};
+
+module.exports = all;
+module.exports.jscs = jscs;
