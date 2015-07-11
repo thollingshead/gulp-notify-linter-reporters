e242f2d9b000665f93179109b5c4dbb8d9832641
Tim Hollingshead <thollingshead@esri.com>
2015-07-11 10:00:15 -0500

    Add jshint reporter

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 var notify = require('gulp-notify');
+var combine = require('stream-combiner2');
 var path = require('path');
 var extend = require('util')._extend;
 var clone = function(obj) {
 	return extend({}, obj);
+};
+
+var formatReport = function(file, errors, options) {
+	options.message = errors.join('\n');
+
+	options.title = file.relative;
+	if (errors.length > 1) {
+		options.title += ' (' + errors.length + ' errors)';
+	}
+
+	return options;
 };
 
 var jscs = function(options) {
@@ -17,20 +29,34 @@ var jscs = function(options) {
 		var errors = file.jscs.errors.map(function(error) {
 			return error.line + ':' + error.column + '  ' + error.message;
 		});
-		options.message = errors.join('\n');
+		return formatReport(file, errors, options);
+	});
+};
 
-		options.title = file.relative;
-		if (errors.length > 1) {
-			options.title += ' (' + errors.length + ' errors)';
+var jshint = function(options) {
+	options = clone(options);
+	options.icon = path.join(__dirname, 'assets', 'jshint.png');
+
+	return notify(function(file) {
+		if (!file.jshint || file.jshint.success) {
+			return;
 		}
 
-		return options;
+		var errors = [];
+		file.jshint.results.forEach(function(data) {
+			if (data.error) {
+				var error = data.error;
+				errors.push(error.line + ':' + error.character + '  ' + error.reason);
+			}
+		});
+		return formatReport(file, errors, options);
 	});
 };
 
 var all = function(options) {
-	return jscs(options);
+	return combine(jscs(options), jshint(options));
 };
 
 module.exports = all;
 module.exports.jscs = jscs;
+module.exports.jshint = jshint;
